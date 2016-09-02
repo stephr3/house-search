ead181c5df739c0997ad4e5cc99d6c864406511b
Stephanie Gurung <stephr3@gmail.com>
2016-09-01 20:51:22 -0700

    remove buy house button if house has already sold

--- app/components/inside-modal.js
+++ app/components/inside-modal.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  housesBought: Ember.inject.service(),
+  alreadySold: Ember.computed('housesBought.boughtHousesList.[]', function(){
+    return this.get('housesBought').includes(this.get('house'));
+  }),
   showForm: false,
   actions: {
     showForm() {
