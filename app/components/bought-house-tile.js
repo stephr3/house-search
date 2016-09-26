256e4d7cb76c607f17436d5064972c532a234eec
Stephanie Gurung <stephr3@gmail.com>
2016-09-25 18:06:09 -0700

    delete all but dist files to deploy app

--- app/components/bought-house-tile.js
+++
@@ -1,5 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend ({
-  housesBought: Ember.inject.service()
-});
--- app/helpers/format-money.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export function formatMoney(params) {
-  var money = params[0];
-  return Ember.String.htmlSafe(money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-}
-
-export default Ember.Helper.helper(formatMoney);
